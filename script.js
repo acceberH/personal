6ae9e6a83f5d40d3812238b5f0f6421df9f1da82
QiongranHuang <qh2130@nyu.edu>
2023-10-05 01:59:24 -0400

    Added original files

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
 const cursor = document.querySelector(".cursor");
 const title = document.querySelector(".title");
 
-document.addEventListener("mousemove", (e) => {
-    cursor.style.top = e.clientY + "px";
-    cursor.style.left = e.clientX + "px";
-
-    const rect = title.getBoundingClientRect();
-    const cursorRect = cursor.getBoundingClientRect();
-
-    if (
-        rect.left < cursorRect.right &&
-        rect.right > cursorRect.left &&
-        rect.top < cursorRect.bottom &&
-        rect.bottom > cursorRect.top
-    ) {
-        cursor.style.backgroundColor = "#fff";
-    } else {
-        cursor.style.backgroundColor = "#000";
-    }
-});
-
+document.body.onmousemove = function(e) {
+    document.documentElement.style.setProperty('--x', (e.clientX + window.scrollX) + 'px');
+    document.documentElement.style.setProperty('--y', (e.clientY + window.scrollY) + 'px');
+}
 document.querySelector(".navbar").addEventListener("mouseenter", () => {
-    cursor.style.width = "40px";
-    cursor.style.height = "40px";
+    cursor.style.width = "80px";  
+    cursor.style.height = "80px";
 });
 
 document.querySelector(".navbar").addEventListener("mouseleave", () => {
-    cursor.style.width = "20px";
-    cursor.style.height = "20px";
+    cursor.style.width = "60px";  
+    cursor.style.height = "60px";
 });
